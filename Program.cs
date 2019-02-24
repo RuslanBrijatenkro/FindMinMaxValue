7526a397cb80eff3ff7ec81f71fbb4e8a77f37f2
RuslanBrijatenkro <yahochuchalic228@gmail.com>
2019-02-24 20:32:10 +0200

    1 lab parral

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace FindMinMax
+{
+    class Program
+    {
+        List<int>
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
